22e574da0b4a2b217c38dd2f4756edf86112fa21
emmanuel tulinayo <oyan.tuli@gmail.com>
2023-07-28 18:14:26 +0200

    step_3

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,22 @@
 //Mettre le code JavaScript lié à la page photographer.html
+// 
+function displayModal(){
+    // classlist.toggle
+}
+function closeModal(){
+    // classlist.toggle
+}
+async function getPhotographer() {
+    // fetch
+}
+async function displayPage() {
+    const photographersheader = document.querySelector(".photographer-header");
+    // photographerTemplate
+}
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers, media } = await getPhotographer();
+    displayPage(photographers);
+}
+// 
+init();
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,89 @@
+// class PhotographerFactory {
+//     constructor (data) {
+//         this.name = data.name
+//         this.portrait = data.portrait
+//     }
+//     // 
+//     getUserCardDOM() {
+//         const picture = `assets/photographers/Photographers ID Photos/${this.portrait}`;
+//         const article = document.createElement( 'article' );
+//         // const anchor = document.createElement( 'a' );
+//         // const label = document.createElement( 'label' );
+//         // const location = document.createElement( 'p' );
+//         // const tagline = document.createElement( 'p' );
+//         // const price = document.createElement( 'p' );
+//         const img = document.createElement( 'img' );
+//         const h2 = document.createElement( 'h2' );
+//         // 
+//         img.setAttribute("src", picture)
+//         img.setAttribute("alt", this.name)
+//         h2.textContent = this.name;
+//         article.addEventListener('click', function (e) {
+//             e.preventDefault()
+//             const url = new URL(window.location.href)
+//             window.location.href = `${url.origin}/Front-End-Fisheye/photographer.html`
+//         })
+//         // 
+//         article.appendChild(img);
+//         article.appendChild(h2);
+//         return (article);
+//     }
+// }
+// 
+// class Photographer extends PhotographerFactory {
+//     super(name)
+// }
+// const photographer = new Photographer("mimi")
+// return photographer.get()
+// 
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
-
+    const { name, portrait, tagline, price, country, city } = data;
+    const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
+    // 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const anchor = document.createElement( 'a' );
+        // const label = document.createElement( 'label' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        const priceText = document.createElement( 'h5' );
+        const taglineText = document.createElement( 'h4' );
+        const locationText = document.createElement( 'h3' );
         const h2 = document.createElement( 'h2' );
+        // 
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", `portrait of ${name}`);
+        article.setAttribute("aria-label", `${name}`)
+        anchor.setAttribute("href", "#")
+        locationText.textContent = `${city}, ${country}.`;
+        taglineText.textContent = tagline;
+        priceText.textContent = `${price} € / jour`;
         h2.textContent = name;
+        // 
+        // article.addEventListener('click', function (e) {
+        //     e.preventDefault()
+        //     const url = new URL(window.locationText.href)
+        //     window.locationText.href = `${url.origin}/Front-End-Fisheye/photographer.html`
+        // })
+        // 
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(locationText);
+        article.appendChild(taglineText);
+        article.appendChild(priceText);
+        anchor.appendChild(article)
+        return (anchor);
     }
-    return { name, picture, getUserCardDOM }
+    //
+    function getUserPageDOM() {
+        // const location = document.createElement( 'p' );
+        // const tagline = document.createElement( 'p' );
+        // const price = document.createElement( 'p' );
+        // const likes = document.createElement( 'p' );
+        const img = document.createElement( 'img' );
+        const h2 = document.createElement( 'h2' );
+        // 
+        img.setAttribute("src", picture)
+        h2.textContent = name;
+    } 
+    return { name, picture, getUserCardDOM, getUserPageDOM }
 }
